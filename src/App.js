cbcade58c9527eaea1ffb398494944fe737e87f7
traut <traute.wes@gmail.com>
2020-04-06 21:21:01 -0400

    Fixing syntax error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <3 Ana
+          For my love - Ana
         </p>
         <a
           className="App-link"
